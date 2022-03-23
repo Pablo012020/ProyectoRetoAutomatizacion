206db26ef21369f52c9ba582dda4bb80f15d60c3
CHBOGML-102323\bancorepublica <emilion04211992@hotmail.com>
2022-03-23 16:30:32 -0500

    target Con evidencia

--- src/main/java/co/com/choucair/certification/proyectoreto/userinterface/MenuPage.java
+++ src/main/java/co/com/choucair/certification/proyectoreto/userinterface/MenuPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class MenuPage extends PageObject {
-    public static final Target MENU_BLOG = Target.the("Selecciona menú Blogs").located(By.xpath("//a[@data-bs-target='#Blog']"));
+    public static final Target MENU_BLOG = Target.the("Selecciona menú Blogs").located(By.xpath("//a[contains(text(),'Blog') and @href='#Blog']"));
     public static final Target SUBMENU_BLOGCATEGORIAS = Target.the("Selecciona submenú Blog Categories").located(By.xpath("//a[contains(text(),'Blog Categories')]"));
     public static final Target SUBMENU_POST = Target.the("Selecciona submenú Post").located(By.xpath("//a[contains(text(),'Post')]"));
 }
